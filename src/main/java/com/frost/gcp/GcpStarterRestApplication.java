fad172390e703519c9bc3d7e36529102742ad07a
jfrost1610 <jobinthomas1610@gmail.com>
2020-03-01 13:34:16 +0530

    first commit

---
+++ src/main/java/com/frost/gcp/GcpStarterRestApplication.java
@@ -0,0 +1,13 @@
+package com.frost.gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpStarterRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpStarterRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frost/gcp/GcpStarterRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frost.gcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpStarterRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
